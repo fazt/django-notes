6427ec6d35364a79f95fa6c51b3aee6e590741a7
SaulCondoriM <104391550+SaulCondoriM@users.noreply.github.com>
2023-12-18 00:59:28 -0500

    Añadiendo la ruta de public_tasks

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -29,4 +29,6 @@ urlpatterns = [
     path('tasks/<int:task_id>', views.task_detail, name='task_detail'),
     path('taks/<int:task_id>/complete', views.complete_task, name='complete_task'),
     path('tasks/<int:task_id>/delete', views.delete_task, name='delete_task'),
+    
+    path('public_tasks/', views.public_tasks, name='public_tasks'),
 ]
