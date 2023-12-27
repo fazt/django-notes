1d41a438ab5904ab1c99ab0f15e717ab48d10f9a
Eduardo Ruiz <eruizm@unsa.edu.pe>
2023-12-26 19:19:59 -0500

    Arregla malas prácticas

--- tasks/tests.py
+++ tasks/tests.py
@@ -4,22 +4,27 @@ from django.urls import reverse
 from .models import Task
 
 class SharedTasksTestCase(TestCase):
+    username_template = 'EGRM23'
+    password_template = 'V6E388KR'
+    title_template = 'Tarea Compartida'
+    description_template = 'Esta es una tarea compartida'
+
     def setUp(self):
-        self.client.login(username='EGRM23', password='V6E388KR')
+        self.client.login(username=self.username_template, password=self.password_template)
 
     def test_create_shared_task(self):
         response = self.client.post(reverse('create_task'), {
-            'title': 'Tarea Compartida',
-            'description': 'Esta es una tarea compartida',
+            'title': self.username_template,
+            'description': self.description_template,
             'shared': True,
         })
 
         self.assertEqual(response.status_code, 302)
-        self.assertTrue(Task.objects.filter(title='Tarea Compartida', shared=True).exists())
+        self.assertTrue(Task.objects.filter(title=self.username_template, shared=True).exists())
 
     def test_view_shared_tasks(self):
         response = self.client.get(reverse('shared_tasks'))
 
         # Verificar que la página se carga correctamente y contiene las tareas compartidas
         self.assertEqual(response.status_code, 200)  # 200 es el código de éxito
-        self.assertContains(response, 'Tarea Compartida')
+        self.assertContains(response, self.username_template)
