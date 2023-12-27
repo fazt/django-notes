a1377310204d17fac2b7c11bded8b639ac798520
Eduardo Ruiz <eruizm@unsa.edu.pe>
2023-12-26 19:54:10 -0500

    Ajusta los casos de prueba a los cmabios hechos

--- tasks/tests.py
+++ tasks/tests.py
@@ -15,14 +15,33 @@ class SharedTasksTestCase(TestCase):
             User.objects.create_user(
                 username=self.username_template,
                 password=self.password_template
-            ) 
+            )
+        
+        users_data = [
+            {'username': 'UsuarioPrueba1', 'password': 'contra1molde'},
+            {'username': 'UsuarioPrueba2', 'password': 'contra2molde'},
+            {'username': 'UsuarioPrueba3', 'password': 'contra3molde'},
+        ]
+
+        for user_data in users_data:
+            user_exists = User.objects.filter(username=user_data['username']).exists()
+            if not user_exists:
+                User.objects.create_user(
+                    username=user_data['username'],
+                    password=user_data['password']
+                )
+        
         self.client.login(username=self.username_template, password=self.password_template)
 
     def test_create_shared_task(self):
+        user1_id = User.objects.get(username='UsuarioPrueba1').id
+        user2_id = User.objects.get(username='UsuarioPrueba2').id
+        
         response = self.client.post(reverse('create_task'), {
             'title': self.username_template,
             'description': self.description_template,
             'shared': True,
+            'shared_with': [user1_id, user2_id]
         })
         self.assertEqual(response.status_code, 302)
         self.assertTrue(Task.objects.filter(title=self.username_template, shared=True).exists())
